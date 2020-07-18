629f31b419effca754eb729622d6cad3befc3e09
Hyunsoo, Chung <admin@hyonsu.com>
2020-07-18 14:11:37 +0900

    Add default admin page redirect

--- routes/admin.js
+++ routes/admin.js
@@ -46,6 +46,8 @@ app.get('/admin/:page', utils.isAdmin, (req, res, next) => {
                 });
             }
             break;
+        default:
+            res.redirect('/admin');
     }
 });
 
